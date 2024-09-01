c483f1155f5802ce69eafdda8ffd64651d0f62de
yesandv <grc@protonmail.com>
2024-09-01 21:09:38 +0300

    Add etl functions

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,22 +1,45 @@
 import sqlite3
+from contextlib import contextmanager
 
 import psycopg
-from psycopg import ClientCursor, connection as _connection
+from psycopg import ClientCursor
 from psycopg.rows import dict_row
 
-
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
-
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+from sqlite_to_postgres.db_settings import TABLE_MAPPING, PG_DSL, SQLITE_DB_PATH
+from sqlite_to_postgres.postgres_saver import PostgresSaver
+from sqlite_to_postgres.sqlite_loader import SQLiteLoader
 
 
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg.connect(
-        **dsl, row_factory=dict_row, cursor_factory=ClientCursor
+@contextmanager
+def sqlite_connection(db_path: str):
+    conn = sqlite3.connect(db_path)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+@contextmanager
+def postgres_connection(dsl: dict, **kwargs):
+    conn = psycopg.connect(**dsl, **kwargs)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def run_etl():
+    with sqlite_connection(SQLITE_DB_PATH) as sqlite_conn, postgres_connection(
+            PG_DSL, row_factory=dict_row, cursor_factory=ClientCursor
     ) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+        sqlite_loader = SQLiteLoader(sqlite_conn)
+        postgres_saver = PostgresSaver(pg_conn)
+
+        for table_name, model in TABLE_MAPPING.items():
+            data = sqlite_loader.extract_data(table_name)
+            transformed_data = sqlite_loader.transform_data(data, model)
+            postgres_saver.load_data(transformed_data, table_name)
+
+
+if __name__ == "__main__":
+    run_etl()
