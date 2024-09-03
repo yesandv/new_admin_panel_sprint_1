14f01ee66641d0c653a40d237765ef079bc75135
yesandv <grc@protonmail.com>
2024-09-03 22:33:47 +0300

    Update admin classes

--- movies_admin/movies_admin/admin.py
+++ movies_admin/movies_admin/admin.py
@@ -5,17 +5,33 @@ from .models import FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork
 
 class GenreFilmWorkInline(admin.TabularInline):
     model = GenreFilmWork
+    autocomplete_fields = ("genre",)
 
 
 class PersonFilmWorkInline(admin.TabularInline):
     model = PersonFilmWork
+    autocomplete_fields = ("person",)
 
 
 @admin.register(FilmWork)
 class FilmWorkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmWorkInline, PersonFilmWorkInline)
     search_fields = ("title", "type", "rating")
-    list_display = ("title", "type", "description", "rating")
+    list_display = ("title", "type", "description", "rating", "get_genres")
+    list_prefetch_related = ("genres",)
+
+    def get_queryset(self, request):
+        queryset = (
+            super()
+            .get_queryset(request)
+            .prefetch_related(*self.list_prefetch_related)
+        )
+        return queryset
+
+    def get_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+
+    get_genres.short_description = "Жанры"
 
 
 @admin.register(Genre)
